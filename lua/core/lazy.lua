092b723f0285d1edc9a51c36fd95f52fedd2c5aa
Liam Vinberg <vinberg.liam@gmail.com>
2025-08-10 12:05:07 +0200

    fix the lazy installation thingy

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -2,7 +2,7 @@ local path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(path) then
 	local repo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=block:none", "--branch=stable", repo, path })
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", repo, path })
 
 	if vim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
